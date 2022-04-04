23d4bee5b0dc49fe05b607fbfa751a91cfb0da4d
sireeshakenguva <sireeshakenguva@gmail.com>
2022-04-04 07:52:09 -0700

    [sireeshakenguva]ADDED implemented daily wage problem in emp wage problem

--- empwage.sh
+++ empwage.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 
+
        Employee=$((RANDOM%2))
+
 
 
        if [ $Employee -eq 1 ];
@@ -10,3 +12,8 @@
        else
        echo "Employee is Absent" $Employee
        fi
+
+Wage_Per_Hour=20
+Full_Day_Hour=8
+Daily_Emp_Wage=$(($Wage_Per_Hour*$Full_Day_Hour))
+echo "Daily Emp Wage is:" $Daily_Emp_Wage
