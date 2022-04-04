16509d420a480e3e48c865bd74dddc83e8c1fb50
sireeshakenguva <sireeshakenguva@gmail.com>
2022-04-04 08:19:52 -0700

    [sireeshakenguva]ADDED implemented switchcase problem in emp wage problem

--- empwage.sh
+++ empwage.sh
@@ -18,4 +18,12 @@ Full_Day_Hour=8
 Daily_Emp_Wage=$(($Wage_Per_Hour*$Full_Day_Hour))
 echo "Daily Emp Wage is:" $Daily_Emp_Wage
 
-Parttime-Hour=4
+Parttime_Hour=4
+
+        case $Employee in
+            1)echo "Daily Employee Wage Full Time - "$DailyEmpWage=$(($Full_Day_Hour*$Wage_Per_Hour));;
+
+            0)echo "Daily Employee Wage Part Time - "$DailyEmpWage=$(($Parttime_Hour*$Wage_Per_Hour));;
+
+            *)echo "wrong choice";;
+        esac
